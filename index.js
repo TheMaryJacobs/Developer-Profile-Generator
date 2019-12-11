ed020dd77f36cebc8e95bac7f09e27b0ac8dc810
Mary Jacobs <TheMaryJacobs@gmail.com>
2019-12-10 18:50:32 -0600

    install all the things

---
+++ index.js
@@ -0,0 +1,118 @@
+
+const axios = require("axios");
+const inquirer = require("inquirer");
+const generatehtml = require("./generateHTML.js")
+
+// var conversion = convertFactory({ converterPath: convertFactory.converters.PDF });
+
+const fs = require('fs');
+const convertFactory = require('electron-html-to');
+
+//console.log(generatehtml);
+let watchers = [];
+
+inquirer.prompt([
+    {
+        type: "input",
+        name: "githubusername",
+        message: "What is your GitHub UserName?"
+    },
+    {
+        type: "checkbox",
+        message: "color?",
+        name: "mycolor",
+        choices: [
+            "green",
+            "blue",
+            "pink",
+            "red"
+        ]
+    }
+]).then(function ({ githubusername, mycolor }) {
+
+    // console.log(githubusername);
+    // console.log(mycolor);
+
+    const queryURL = `https://api.github.com/users/${githubusername}/repos?per_page=100`;
+    const queryURL2 = `https://api.github.com/users/${githubusername}`;
+
+    axios.get(queryURL2)
+        .then(function (res) {
+
+            let githubandcolorinfo = {
+                color: mycolor,
+                profileimageurl: res.data.avatar_url,
+                username: res.data.name,
+                location: res.data.location,
+                profilelink: res.data.html_url,
+                blog: res.data.blog,
+                bio: res.data.bio,
+                publicrepos: res.data.public_repos,
+                followers: res.data.followers,
+                following: res.data.following,
+            };
+            // * Profile image
+            //console.log("profile image" + res.data.avatar_url);
+            // * User name
+            //console.log("username" + res.data.login);
+            // * Links to the following:
+            //     * User location via Google Maps
+            //console.log("location" + res.data.location);
+            //     * User GitHub profile
+            //console.log("profile link" + res.data.html_url)
+            //     * User blog
+            //console.log("blog" + res.data.blog);
+            // * User bio
+            //console.log("bio" + res.data.bio);
+            // * Number of public repositories
+            //console.log("public repos" + res.data.public_repos);
+            // * Number of followers
+            //console.log("followers" + res.data.followers);
+            // * Number of users following
+            //console.log("following" + res.data.following);
+
+            axios.get(queryURL)
+                .then(function (res) {
+                    for (let i = 0; i < res.data.length; i++) {
+                        watchers.push(res.data[i].watchers_count);
+                    }
+                    console.log(watchers);
+                    let totalwatchers = watchers.reduce((a, b) => a + b);
+
+                    //  Number of GitHub stars
+                    console.log("watches/stars" + totalwatchers);
+
+                    // fs.writeFile("myselection.txt",)
+                    githubandcolorinfo.stars = totalwatchers;
+                    // console.log(githubandcolorinfo);
+                    generatehtml(githubandcolorinfo);
+
+
+
+                    fs.writeFile("resume.html", generatehtml(githubandcolorinfo), function (err) {
+                        if (err) {
+                            return console.log(err);
+                        }
+                        console.log("Done!");
+                    })
+
+
+                    fs.readFile('resume.html', 'utf8', (err, htmlString) => {
+                        // add local path in case your HTML has relative paths
+                        htmlString = htmlString.replace(/href="|src="/g, match => {
+                            return match + './';
+                        });
+                        const conversion = convertFactory({
+                            converterPath: convertFactory.converters.PDF,
+                            allowLocalFilesAccess: true
+                        });
+                        conversion({ html: htmlString }, (err, result) => {
+                            if (err) return console.error(err);
+                            result.stream.pipe(fs.createWriteStream('./resume.pdf'));
+                            conversion.kill(); 
+                        });
+                    });
+
+                });
+        })
+})
